393ef327738eea5ea3c1b80b44ac90f4d259564f
Łukasz Bacik <mail@luka.sh>
2023-11-09 11:20:39 +0100

    Refactor - formatting

--- src/routes.js
+++ src/routes.js
@@ -2,36 +2,34 @@
 const GlsWeb = require('./gls/gls-web')
 
 module.exports = (app, glsWeb) => {
+  const LOCAL_URI = '/lo'
+  const DB_URI = '/db'
 
-    const LOCAL_URI = '/lo'
-    const DB_URI = '/db'
+  app.get('/', (req, res) => {
+    res.status(301).redirect(`${LOCAL_URI}`)
+  })
 
-    app.get('/', (req, res) => {
-        res.status(301).redirect(`${LOCAL_URI}`)
+  app.get('/code-generator', (req, res) => {
+    res.render('data', {
+      data: GlsWeb.code(),
     })
+    res.end()
+  })
 
-    app.get('/code-generator', (req, res) => {
-        res.render('data', {
-            data: GlsWeb.code()
-        })
-        res.end()
-    })
+  app.get(`${LOCAL_URI}/:name?`, (req, res) => {
+    const data = glsWeb.local(req.params.name)
+    data.uri = LOCAL_URI
+    res.render('canvas', data)
+    res.end()
+  })
 
-    app.get(`${LOCAL_URI}/:name?`, (req, res) => {
-        const data = glsWeb.local(req.params.name)
-        data.uri = LOCAL_URI
+  app.get(`${DB_URI}/:name?`, (req, res) => {
+    glsWeb
+      .db(req.params.name)
+      .then((data) => {
+        data.uri = DB_URI
         res.render('canvas', data)
         res.end()
-    })
-
-    app.get(`${DB_URI}/:name?`, (req, res) => {
-
-        glsWeb
-            .db(req.params.name)
-            .then(data => {
-                data.uri = DB_URI
-                res.render('canvas', data)
-                res.end()
-            })
-    })
+      })
+  })
 }
